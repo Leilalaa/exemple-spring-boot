3ac939ab5ae08ac8e67da61b0210f13f6090b6e2
LeilaLaa <aaleila19@gmail.com>
2018-02-07 17:40:41 +0100

    TP 06 Spring Boot

---
+++ src/main/java/dev/exemple/repository/ExempleRepository.java
@@ -0,0 +1,10 @@
+package dev.exemple.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import dev.exemple.entite.Exemple;
+
+
+public interface ExempleRepository extends JpaRepository<Exemple, Integer>{
+
+}
